bca4002ca2593f73a79fe7454f3a9e64c00d21b5
Stiliyan <stilian.kocev@gmail.com>
2015-09-30 19:53:26 +0300

    Class was taken on separate file

---
+++ Space-Bridge-Test/Object.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Bridge_Test
+{
+    class Object
+    {
+        private int x;
+        private int y;
+        private ConsoleColor color;
+        private string symbol;
+
+        public Object(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
+        {
+
+            this.X = x;
+            this.Y = y;
+            this.Symbol = symbol;
+            this.Color = color;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Symbol { get; set; }
+        public ConsoleColor Color { get; set; }
+    }
+}
--- Space-Bridge-Test/SpaceBridgeTest.cs
+++ Space-Bridge-Test/SpaceBridgeTest.cs
@@ -2,143 +2,124 @@
 using System.Collections.Generic;
 using System.Threading;
 
-public class GameTeamWork
+namespace Space_Bridge_Test
 {
-    class Object
+    public class SpaceBridge
     {
-        private int x;
-        private int y;
-        private ConsoleColor color;
-        private string symbol;
-
-        public Object(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
+        public static void Main()
         {
-
-            this.X = x;
-            this.Y = y;
-            this.Symbol = symbol;
-            this.Color = color;
-        }
-
-        public int X { get; set ; }
-        public int Y { get; set; }
-        public string Symbol { get; set; }
-        public ConsoleColor Color { get; set; }
-    }
-
-    public static void Main()
-    {
-        int Points = 0;
-        //2.Draw playfield
-        int playfieldWidth = 35;
-        Console.BufferHeight = Console.WindowHeight = 10;//This is the size of the console window height.
-        Console.BufferWidth = Console.WindowWidth = 49;//size of window width.                              
-        Console.BackgroundColor = ConsoleColor.Black;//color of playfield
-        //3.Make object
-        int x = 23;
-        int y = Console.WindowHeight - 3;
-        string symbol = "___";
-        ConsoleColor color = ConsoleColor.DarkRed;
-        Object userObject = new Object(x, y, symbol, color);
-        Object newObject = new Object(9, 0, "$", ConsoleColor.Green);
-        bool bridgeHitted = false;
-        while (true)
-        {
-            //4.Move the bridge
-            while (Console.KeyAvailable)
+            int Points = 0;
+            //2.Draw playfield
+            int playfieldWidth = 35;
+            Console.BufferHeight = Console.WindowHeight = 10;//This is the size of the console window height.
+            Console.BufferWidth = Console.WindowWidth = 49;//size of window width.                              
+            Console.BackgroundColor = ConsoleColor.Black;//color of playfield
+                                                         //3.Make object
+            int x = 23;
+            int y = Console.WindowHeight - 3;
+            string symbol = "___";
+            ConsoleColor color = ConsoleColor.DarkRed;
+            Object userObject = new Object(x, y, symbol, color);
+            Object newObject = new Object(9, 0, "$", ConsoleColor.Green);
+            bool bridgeHitted = false;
+            while (true)
             {
-                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                //4.Move the bridge
                 while (Console.KeyAvailable)
                 {
-                    Console.ReadKey(true);
-                }
-                if (pressedKey.Key == ConsoleKey.LeftArrow)
-                {
-                    if (userObject.X > 15)
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                    while (Console.KeyAvailable)
                     {
-                        userObject.X -= 9;
+                        Console.ReadKey(true);
+                    }
+                    if (pressedKey.Key == ConsoleKey.LeftArrow)
+                    {
+                        if (userObject.X > 15)
+                        {
+                            userObject.X -= 9;
+                        }
+                    }
+                    else if (pressedKey.Key == ConsoleKey.RightArrow)
+                    {
+                        if (userObject.X + 1 < playfieldWidth - 7)
+                        {
+                            userObject.X += 9;
+                        }
                     }
                 }
-                else if (pressedKey.Key == ConsoleKey.RightArrow)
+                //Moving our alien....
+                if (!bridgeHitted)
                 {
-                    if (userObject.X + 1 < playfieldWidth - 7)
+                    if (newObject.Y < Console.WindowHeight)
                     {
-                        userObject.X += 9;
+                        newObject.Y++;
+                    }
+                    if (newObject.X < Console.WindowWidth)
+                    {
+                        if (newObject.X == 15)
+                        {
+                            newObject.X--;
+                        }
+                        newObject.X++;
+                    }
+                    if (newObject.Y == userObject.Y && newObject.X == userObject.X + 1)
+                    {
+                        Points++;
+                        bridgeHitted = true;
                     }
                 }
-            }
-            //Moving our alien....
-            if (!bridgeHitted)
-            {
-                if (newObject.Y < Console.WindowHeight)
+                if (bridgeHitted)
                 {
-                    newObject.Y++;
-                }
-                if (newObject.X < Console.WindowWidth)
-                {
-                    if (newObject.X == 15)
+                    if (newObject.Y > Console.WindowHeight - 7)
                     {
-                        newObject.X--;
+                        newObject.Y--;
                     }
-                    newObject.X++;
+                    else
+                    {
+                        bridgeHitted = false;
+                    }
+                    if (newObject.X < Console.WindowWidth)
+                    {
+                        newObject.X++;
+                    }
                 }
-                if (newObject.Y == userObject.Y && newObject.X == userObject.X + 1)
+                //remove objects which are outside the field and create new
+                if (newObject.X > 42)
                 {
-                    Points++;
-                    bridgeHitted = true;
+                    newObject = new Object(9, 0, "$", ConsoleColor.Green);
                 }
-            }
-            if (bridgeHitted)
-            {
-                if (newObject.Y > Console.WindowHeight - 7)
-                {
-                    newObject.Y--;
-                }
-                else
-                {
-                    bridgeHitted = false;
-                }
-                if (newObject.X < Console.WindowWidth)
-                {
-                    newObject.X++;
-                }
-            }
-            //remove objects which are outside the field and create new
-            if (newObject.X > 42)
-            {
-                newObject = new Object(9, 0, "$", ConsoleColor.Green);
-            }
-            //7.Clear the console with 
-            Console.Clear();
-            //Print other object
-            PrintOnPosition(newObject.X, newObject.Y, newObject.Symbol, newObject.Color);
-            //8.print our object
-            PrintOnPosition(userObject.X, userObject.Y, userObject.Symbol, userObject.Color);
-            //printing points
-            PrintOnPosition(41, 0, "P:" + Points.ToString(), ConsoleColor.DarkRed);
-            //Print board for the field
-            PrintTheSideBoard(8);
-            PrintTheSideBoard(40);
+                //7.Clear the console with 
+                Console.Clear();
+                //Print other object
+                PrintOnPosition(newObject.X, newObject.Y, newObject.Symbol, newObject.Color);
+                //8.print our object
+                PrintOnPosition(userObject.X, userObject.Y, userObject.Symbol, userObject.Color);
+                //printing points
+                PrintOnPosition(41, 0, "P:" + Points.ToString(), ConsoleColor.DarkRed);
+                //Print board for the field
+                PrintTheSideBoard(8);
+                PrintTheSideBoard(40);
 
-            //9.Draw info
-            //10.Slow down program
-            Thread.Sleep(500);
+                //9.Draw info
+                //10.Slow down program
+                Thread.Sleep(500);
+            }
         }
-    }
 
-    private static void PrintTheSideBoard(int x)
-    {
-        for (int i = 0; i < Console.WindowHeight; i++)
+        private static void PrintTheSideBoard(int x)
         {
-            PrintOnPosition(x, i, "|", ConsoleColor.White);
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                PrintOnPosition(x, i, "|", ConsoleColor.White);
+            }
         }
-    }
-    //Method which print object 
-    static void PrintOnPosition(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
-    {
-        //Console.SetCursorPosition move our cursor in place of what we write.
-        Console.SetCursorPosition(x, y);
-        Console.ForegroundColor = color;
-        Console.Write(symbol);
+        //Method which print object 
+        static void PrintOnPosition(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
+        {
+            //Console.SetCursorPosition move our cursor in place of what we write.
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(symbol);
+        }
     }
 }
