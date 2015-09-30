ff50d7faf9984f03688d11e51802158c8c91fb99
Stiliyan <stilian.kocev@gmail.com>
2015-09-30 19:48:41 +0300

    Removed unnecessary class.The rebound function was added.

--- Space-Bridge-Test/SpaceBridgeTest.cs
+++ Space-Bridge-Test/SpaceBridgeTest.cs
@@ -1,31 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Threading;
 
-public class SpaceBridgeTest
+public class GameTeamWork
 {
-    public class UserObject
-    {
-        private int x;
-        private int y;
-        private ConsoleColor color;
-        private string symbol;
-
-        public UserObject(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
-        {
-            this.X = x;
-            this.Y = y;
-            this.Symbol = symbol;
-            this.Color = color;
-        }
-
-        public int X { get; set; }
-        public int Y { get; set; }
-        public string Symbol { get; set; }
-        public ConsoleColor Color { get; set; }
-    }
-    public class Object
+    class Object
     {
         private int x;
         private int y;
@@ -41,7 +20,7 @@ public class SpaceBridgeTest
             this.Color = color;
         }
 
-        public int X { get; set; }
+        public int X { get; set ; }
         public int Y { get; set; }
         public string Symbol { get; set; }
         public ConsoleColor Color { get; set; }
@@ -49,31 +28,23 @@ public class SpaceBridgeTest
 
     public static void Main()
     {
-        int points = 0;
+        int Points = 0;
         //2.Draw playfield
         int playfieldWidth = 35;
         Console.BufferHeight = Console.WindowHeight = 10;//This is the size of the console window height.
         Console.BufferWidth = Console.WindowWidth = 49;//size of window width.                              
         Console.BackgroundColor = ConsoleColor.Black;//color of playfield
-
         //3.Make object
         int x = 23;
         int y = Console.WindowHeight - 3;
         string symbol = "___";
         ConsoleColor color = ConsoleColor.DarkRed;
-        UserObject userObject = new UserObject(x, y, symbol, color);
-
-        List<Object> objects = new List<Object>();
-        Random randomGenerator = new Random();
+        Object userObject = new Object(x, y, symbol, color);
+        Object newObject = new Object(9, 0, "$", ConsoleColor.Green);
+        bool bridgeHitted = false;
         while (true)
         {
-            bool hit = false;
-
-            int xx = 9;
-            Object newObject = new Object(xx, 0, "$", ConsoleColor.Green);
-            objects.Add(newObject);
-
-            //4.Move our object
+            //4.Move the bridge
             while (Console.KeyAvailable)
             {
                 ConsoleKeyInfo pressedKey = Console.ReadKey(true);
@@ -96,68 +67,72 @@ public class SpaceBridgeTest
                     }
                 }
             }
-
-            if (objects[0].Y < Console.WindowHeight)
+            //Moving our alien....
+            if (!bridgeHitted)
             {
-                objects[0].Y = objects[0].Y + 1;
-            }
-            if (objects[0].X < Console.WindowWidth)
-            {
-                if (objects[0].X == 15)
+                if (newObject.Y < Console.WindowHeight)
                 {
-                    objects[0].X--;
+                    newObject.Y++;
                 }
-                objects[0].X++;
-            }
-            if (objects.Count > 0)
-            {
-                if (objects[0].Y == userObject.Y && objects[0].X == userObject.X + 1)
+                if (newObject.X < Console.WindowWidth)
                 {
-                    points++;
+                    if (newObject.X == 15)
+                    {
+                        newObject.X--;
+                    }
+                    newObject.X++;
+                }
+                if (newObject.Y == userObject.Y && newObject.X == userObject.X + 1)
+                {
+                    Points++;
+                    bridgeHitted = true;
                 }
             }
-            // Check for HIT!!!
-
+            if (bridgeHitted)
+            {
+                if (newObject.Y > Console.WindowHeight - 7)
+                {
+                    newObject.Y--;
+                }
+                else
+                {
+                    bridgeHitted = false;
+                }
+                if (newObject.X < Console.WindowWidth)
+                {
+                    newObject.X++;
+                }
+            }
+            //remove objects which are outside the field and create new
+            if (newObject.X > 42)
+            {
+                newObject = new Object(9, 0, "$", ConsoleColor.Green);
+            }
             //7.Clear the console with 
             Console.Clear();
             //Print other object
-            foreach (Object element in objects)
-            {
-                PrintOnPosition(element.X, element.Y, element.Symbol, element.Color);
-            }
+            PrintOnPosition(newObject.X, newObject.Y, newObject.Symbol, newObject.Color);
             //8.print our object
             PrintOnPosition(userObject.X, userObject.Y, userObject.Symbol, userObject.Color);
-            //printing userObject lives
-
-            PrintOnPosition(41, 0, "P:" + points.ToString(), ConsoleColor.DarkRed);
+            //printing points
+            PrintOnPosition(41, 0, "P:" + Points.ToString(), ConsoleColor.DarkRed);
             //Print board for the field
-            for (int i = 0; i < Console.WindowHeight; i++)
-            {
-                PrintOnPosition(8, i, "|", ConsoleColor.White);
-            }
-            PrintTheSideBoard();
-            //remove objects which are outside the field
-            if (objects.Count > 0)
-            {
-                if (objects[0].Y == Console.WindowHeight - 1)
-                {
-                    objects.RemoveAt(0);
-                }
-            }
+            PrintTheSideBoard(8);
+            PrintTheSideBoard(40);
+
             //9.Draw info
             //10.Slow down program
-            Thread.Sleep(300);
+            Thread.Sleep(500);
         }
     }
 
-    private static void PrintTheSideBoard()
+    private static void PrintTheSideBoard(int x)
     {
         for (int i = 0; i < Console.WindowHeight; i++)
         {
-            PrintOnPosition(40, i, "|", ConsoleColor.White);
+            PrintOnPosition(x, i, "|", ConsoleColor.White);
         }
     }
-
     //Method which print object 
     static void PrintOnPosition(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
     {
